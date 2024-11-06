864c8418d4b56fd12bfeeee9870bbda5720437ee
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2024-11-06 11:33:32 -0500

    feat: stub out configs based on requirements (#5)

--- config.go
+++ config.go
@@ -2,8 +2,10 @@ package authmeter
 
 import (
 	"errors"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 // Config defines the config for middleware.
@@ -12,6 +14,73 @@ type Config struct {
 	// Optional. Default: nil
 	Next func(*fiber.Ctx) bool
 
+	LimitConfig
+	CreditConfig
+	ScopeConfig
+	KeyAuthConfig
+}
+
+type LimitConfig struct {
+	// Max number of recent connections during `Expiration` seconds before sending a 429 response
+	//
+	// Default: 5
+	Max int
+
+	// KeyGenerator allows you to generate custom keys, by default c.IP() is used
+	//
+	// Default: func(c *fiber.Ctx) string {
+	//   return c.IP()
+	// }
+	KeyGenerator func(*fiber.Ctx) string
+
+	// Expiration is the time on how long to keep records of requests in memory
+	//
+	// Default: 1 * time.Minute
+	Expiration time.Duration
+
+	// LimitReached is called when a request hits the limit
+	//
+	// Default: func(c *fiber.Ctx) error {
+	//   return c.SendStatus(fiber.StatusTooManyRequests)
+	// }
+	LimitReached fiber.Handler
+
+	// When set to true, requests with StatusCode >= 400 won't be counted.
+	//
+	// Default: false
+	SkipFailedRequests bool
+
+	// When set to true, requests with StatusCode < 400 won't be counted.
+	//
+	// Default: false
+	SkipSuccessfulRequests bool
+
+	// Store is used to store the state of the middleware
+	//
+	// Default: an in memory store for this process only
+	Storage fiber.Storage
+
+	// LimiterMiddleware is the struct that implements a limiter middleware.
+	//
+	// Default: a new Fixed Window Rate Limiter
+	LimiterMiddleware limiter.LimiterHandler
+}
+
+type CreditConfig struct {
+	Storage fiber.Storage
+
+	GetCredits func(fiber.Storage, string) (int, error)
+
+	DeductCredits func(fiber.Storage, string, int) error
+}
+
+type ScopeConfig struct {
+	Storage fiber.Storage
+
+	Allow func(c *fiber.Ctx, storage fiber.Storage, key string) (bool, error)
+}
+
+type KeyAuthConfig struct {
 	// SuccessHandler defines a function which is executed for a valid key.
 	// Optional. Default: nil
 	SuccessHandler fiber.Handler
@@ -46,18 +115,40 @@ type Config struct {
 
 // ConfigDefault is the default config
 var ConfigDefault = Config{
-	SuccessHandler: func(c *fiber.Ctx) error {
-		return c.Next()
+	KeyAuthConfig: KeyAuthConfig{
+		SuccessHandler: func(c *fiber.Ctx) error {
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			if errors.Is(err, ErrMissingOrMalformedAPIKey) {
+				return c.Status(fiber.StatusUnauthorized).SendString(err.Error())
+			}
+			return c.Status(fiber.StatusUnauthorized).SendString("Invalid or expired API Key")
+		},
+		KeyLookup:  "header:" + fiber.HeaderAuthorization,
+		AuthScheme: "Bearer",
+		ContextKey: "token",
 	},
-	ErrorHandler: func(c *fiber.Ctx, err error) error {
-		if errors.Is(err, ErrMissingOrMalformedAPIKey) {
-			return c.Status(fiber.StatusUnauthorized).SendString(err.Error())
-		}
-		return c.Status(fiber.StatusUnauthorized).SendString("Invalid or expired API Key")
+
+	// TODO
+	CreditConfig: CreditConfig{},
+
+	// TODO
+	ScopeConfig: ScopeConfig{},
+
+	LimitConfig: LimitConfig{
+		Max:        5,
+		Expiration: 1 * time.Minute,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.SendStatus(fiber.StatusTooManyRequests)
+		},
+		SkipFailedRequests:     false,
+		SkipSuccessfulRequests: false,
+		LimiterMiddleware:      limiter.FixedWindow{},
 	},
-	KeyLookup:  "header:" + fiber.HeaderAuthorization,
-	AuthScheme: "Bearer",
-	ContextKey: "token",
 }
 
 // Helper function to set default values
@@ -71,6 +162,8 @@ func configDefault(config ...Config) Config {
 	cfg := config[0]
 
 	// Set default values
+
+	// keyauth
 	if cfg.SuccessHandler == nil {
 		cfg.SuccessHandler = ConfigDefault.SuccessHandler
 	}
@@ -85,10 +178,34 @@ func configDefault(config ...Config) Config {
 		}
 	}
 	if cfg.Validator == nil {
-		panic("fiber: keyauth middleware requires a validator function")
+		panic("fiber: authmeter middleware requires a validator function")
 	}
 	if cfg.ContextKey == nil {
 		cfg.ContextKey = ConfigDefault.ContextKey
+	}
+
+	// TODO: credit
+	//
+	// TODO: scope
+
+	// limit
+	if cfg.Next == nil {
+		cfg.Next = ConfigDefault.Next
+	}
+	if cfg.Max <= 0 {
+		cfg.Max = ConfigDefault.Max
+	}
+	if int(cfg.Expiration.Seconds()) <= 0 {
+		cfg.Expiration = ConfigDefault.Expiration
+	}
+	if cfg.KeyGenerator == nil {
+		cfg.KeyGenerator = ConfigDefault.KeyGenerator
+	}
+	if cfg.LimitReached == nil {
+		cfg.LimitReached = ConfigDefault.LimitReached
+	}
+	if cfg.LimiterMiddleware == nil {
+		cfg.LimiterMiddleware = ConfigDefault.LimiterMiddleware
 	}
 
 	return cfg
